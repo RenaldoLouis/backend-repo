071e162204afc00c93a4a20645ddb84f56a9e7a8
RenaldoLouis <renald@10xers.co>
2024-05-31 21:38:16 +0700

    databse url

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -1,6 +1,7 @@
 const serviceAccount = require("../../ebuddy-4878b-firebase-adminsdk-qz5ug-dba71e65f9.json");
 const { initializeApp, cert } = require("firebase-admin/app");
 
+// ebuddy-4878b.firebaseio.com
 initializeApp({
     credential: cert(serviceAccount),
     databaseURL: process.env.databaseURL
